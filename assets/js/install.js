484c4dc1252203ef09228c3d005b5bdcb3a7891b
das_kultural <daskultural@gmail.com>
2020-12-04 17:15:48 -0300

    prueba 3 botón instalar - index.html - sw.js - install.js

--- assets/js/install.js
+++ assets/js/install.js
@@ -1,30 +1,59 @@
-`use strict`;
+'use strict';
 
 let deferredInstallPrompt = null;
-const installButton = document.getElementById(`instalar`);
-installButton.aaddEventListener(`beforeinstallprompt`, saveBeforeInstalPpromptEvent);
+const installButton = document.getElementById('butInstall');
+installButton.addEventListener('click', installPWA);
 
-function saveBeforInstallPromptEvent(evt){
- deferredInstallPrompt = evt;
-  installButton.removeAttribute(`hidden`);
+// CODELAB: Add event listener for beforeinstallprompt event
+window.addEventListener('beforeinstallprompt', saveBeforeInstallPromptEvent);
+
+
+/**
+ * Event handler for beforeinstallprompt event.
+ *   Saves the event & shows install button.
+ *
+ * @param {Event} evt
+ */
+function saveBeforeInstallPromptEvent(evt) {
+  // CODELAB: Add code to save event & show the install button.
+  deferredInstallPrompt = evt;
+  installButton.removeAttribute('hidden');
 }
 
-function installPWA(evt){
+
+/**
+ * Event handler for butInstall - Does the PWA installation.
+ *
+ * @param {Event} evt
+ */
+function installPWA(evt) {
+  // CODELAB: Add code show install prompt & hide the install button.
   deferredInstallPrompt.prompt();
-  evt.srcElement.setAttribute(`hidden`, true);
-  deferredInstallPrompt.userChoice.then((choice)=>{
-    if(choice.outcome === "accepted"){
-      console.log("aceptado")
-    } else {
-      console.log("no aceptado")
-    )
-    deferredInstallPrompt = null;
-  })
-
+  // Hide the install button, it can't be called twice.
+  evt.srcElement.setAttribute('hidden', true);
+  // CODELAB: Log user response to prompt.
+  deferredInstallPrompt.userChoice
+      .then((choice) => {
+        if (choice.outcome === 'accepted') {
+          console.log('aceptado', choice);
+        } else {
+          console.log('no aceptado', choice);
+        }
+        deferredInstallPrompt = null;
+      });
 }
 
-window.addEventListener(`appinstalled`, logAppInstalled);
+// CODELAB: Add event listener for appinstalled event
+window.addEventListener('appinstalled', logAppInstalled);
 
-function logAppInstalled(evt){
-  console.log("¡Listo!");
+/**
+ * Event handler for appinstalled event.
+ *   Log the installation to analytics or save the event somehow.
+ *
+ * @param {Event} evt
+ */
+function logAppInstalled(evt) {
+  // CODELAB: Add code to log the event
+  console.log('¡Listo!', evt);
+
 }
